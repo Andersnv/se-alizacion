7757e1f6a19c40b141b7c6561d50bd08a210e849
Andersnv <122251239+Andersnv@users.noreply.github.com>
2023-01-08 23:42:25 -0600

    Add files via upload

---
+++ varo/script.js
@@ -0,0 +1,18 @@
+jQuery("document") .ready(function($){
+
+var menuBtn = $('.menuicon'),
+menu = $('.navigation ul');
+ 
+ menuBt.click(function(){
+ 	if (menu.hasClass('show')) {
+ 		menu.removeClass('show')
+ 	} else{
+ 		menu.addClass('show')
+ 	}
+ 	menu.addClass('show');
+
+
+ });
+
+});
+
